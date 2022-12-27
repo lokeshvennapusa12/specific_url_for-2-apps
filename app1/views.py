746b1aa94cd01c843037a90c13c13e4e34adf9f3
lokeshvennapusa12 <lokeshvennapusa12@gmail.com>
2022-12-27 15:32:49 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def first(request):
-    return HttpResponse('This is my first project in specific urls in app1')
+    return HttpResponse('<h1>This is my first project in specific urls in app1</h1>')
--- app2/views.py
+++ app2/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def second(request):
+    return HttpResponse('<h1> This is my first project in specific url app2</h1>')
